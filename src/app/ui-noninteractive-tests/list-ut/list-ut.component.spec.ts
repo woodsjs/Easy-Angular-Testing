08619b21321ce3ce7acf589f3c01e5207b5a213a
Jason Woods <jason.woods@datatri.be>
2019-11-19 06:43:26 -0600

    We broke some tests while playing, so we're starting to fix those tests.

--- src/app/ui-noninteractive-tests/list-ut/list-ut.component.spec.ts
+++ src/app/ui-noninteractive-tests/list-ut/list-ut.component.spec.ts
@@ -1,30 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatListModule } from '@angular/material/list';
 
 import { ListUtComponent } from './list-ut.component';
+import { DebugElement } from '@angular/core';
 
 describe('ui-noninteractive - ListUtComponent', () => {
   let component: ListUtComponent;
   let fixture: ComponentFixture<ListUtComponent>;
 
-  const listItems = [
+  const landAnimals = [
     {
-      name: 'item one',
-      description: 'This is item one',
+      name: 'Blue Bull',
+      description: 'This is a Blue Bull',
       avatar: 'https://commons.wikimedia.org/wiki/File:1000BlueBull.jpg',
       category: 'land'
     },
     {
-      name: 'item two',
-      description: 'This is item two',
+      name: 'Cervus',
+      description: 'This is a Cervus',
       avatar: 'https://commons.wikimedia.org/wiki/File:102Cervus.jpg',
       category: 'land'
     },
     {
-      name: 'item three',
-      description: 'This is item three',
+      name: 'Black Buck',
+      description: 'This is a Black Buck',
       avatar: 'https://commons.wikimedia.org/wiki/File:10BlackBuck.jpg',
       category: 'land'
-    },
+    }
+  ];
+
+  const airAnimals = [
     {
       name: 'item four',
       description: 'This is item four',
@@ -36,18 +41,26 @@ describe('ui-noninteractive - ListUtComponent', () => {
       description: 'This is item five',
       avatar:
         'https://commons.wikimedia.org/wiki/File:2005-malabar-parkeet-p.jpg',
-        category: 'air'
+      category: 'air'
     }
   ];
 
+  const listItems = [];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [MatListModule],
       declarations: [ListUtComponent]
     }).compileComponents();
   }));
 
   beforeEach(() => {
+    // we split our array into the categories we're going to test for,
+    // this way we have something to run our expect call against without
+    // actually coding the filter here.  We want to write as little code
+    // as possible in our tests!
+    listItems.concat(landAnimals, airAnimals);
+
     fixture = TestBed.createComponent(ListUtComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -60,23 +73,42 @@ describe('ui-noninteractive - ListUtComponent', () => {
   // let's have an array of items with different categories
   // when a UI button is clicked, it shows the appropriate list
   it('should create a list with land animals', () => {
+    const buttonDe: DebugElement = fixture.debugElement;
+    const buttonEl: HTMLElement = buttonDe.nativeElement;
+    const button = buttonEl.querySelector('button');
 
-    const landAnimals = listItems.filter((animal) => animal.category === 'land' );
-    console.log(landAnimals);
+    const animalType = 'land';
+
     // we would most likely get our data from a service, either to retreive
     // it from a DB or a web API.  Here were smushing that into a call to
     // getLandAnimals in the same component for show.
-    spyOn(component, 'getLandAnimals').and.returnValue(listItems);
+    // but this mocked call could be the same!
+    spyOn(component, 'getLandAnimals').and.returnValue(landAnimals);
 
     // this method will call the getLandAnimals method, which will be intercepted above
-    component.showFilteredAnimals('land');
+    button.click();
+    fixture.detectChanges();
+
+    // find our mat list. It's in our best interest to add an id to elements
+    // so in cases like this, and styling, we can find that element.
+    fixture.whenStable().then(() => {
+      const ourDomListUnderTest = document.querySelector(
+        'mat-list#' + animalType
+      );
+
+      // here is where we would put our expect clause to look at the children,
+      // then pick the attributes and test the array against those attributes
+      ourDomListUnderTest.childNodes.forEach(matListItem =>
+        matListItem.childNodes.forEach(item => console.log("ze item" + item.textContent))
+      );
+    });
 
     // we need to check between the displayed list and what we have in landAnimals
     // we could just check between landAnimals and the output of showFilteredAnimals but
     // that really does not meet our needs
-
   });
 
   it('should show the user the correct avatar', () => {});
 
+  it('should show a list with subheaders based on category', () => {});
 });
--- src/app/ui-noninteractive-tests/list-ut/list-ut.component.ts
+++ src/app/ui-noninteractive-tests/list-ut/list-ut.component.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListUtComponent implements OnInit {
 
-  constructor() { }
+  // this would be the data sent back from our API. Since we're doing this for the 
+  // tests we're skipping the service here.
+  apiReturnedAnimals = [
+    {
+      name: 'Blue Bull',
+      description: 'This is a Blue Bull',
+      avatar: 'https://commons.wikimedia.org/wiki/File:1000BlueBull.jpg',
+      category: 'land'
+    },
+    {
+      name: 'Cervus',
+      description: 'This is a Cervus',
+      avatar: 'https://commons.wikimedia.org/wiki/File:102Cervus.jpg',
+      category: 'land'
+    },
+    {
+      name: 'Black Buck',
+      description: 'This is a Black Buck',
+      avatar: 'https://commons.wikimedia.org/wiki/File:10BlackBuck.jpg',
+      category: 'land'
+    },
+    {
+      name: 'item four',
+      description: 'This is item four',
+      avatar: 'https://commons.wikimedia.org/wiki/File:3617brahminy-mynah.jpg',
+      category: 'air'
+    },
+    {
+      name: 'item five',
+      description: 'This is item five',
+      avatar:
+        'https://commons.wikimedia.org/wiki/File:2005-malabar-parkeet-p.jpg',
+      category: 'air'
+    }
+  ];
 
-  ngOnInit() {
+  public listData;
+  public listType;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  getLandAnimals( animalType: string ) {
+    return this.apiReturnedAnimals.filter((animal) => animal.category === animalType );
   }
 
+  showFilteredAnimals( animalType: string ) {
+    // this is where we build pass our data to our list
+    this.listData = this.getLandAnimals(animalType);
+    this.listType = animalType;
+  }
 }
