49ff48155f582215be3e5d87930e56b1239bd773
Jason Woods <jason.woods@datatri.be>
2019-11-29 07:10:26 -0600

    Started working through dialog tests. Have our cases and some test data.

--- src/app/ui-noninteractive-tests/dialog-ut/dialog-ut.component.spec.ts
+++ src/app/ui-noninteractive-tests/dialog-ut/dialog-ut.component.spec.ts
@@ -1,16 +1,20 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { DialogUtComponent } from './dialog-ut.component';
+import { DialogUtComponent } from "./dialog-ut.component";
 
-describe('ui-noninteractive - DialogUtComponent', () => {
+describe("ui-noninteractive - DialogUtComponent", () => {
   let component: DialogUtComponent;
   let fixture: ComponentFixture<DialogUtComponent>;
 
+  const testItems = ["thing one", "thing two", "thing three"];
+
+  const itemToDelete = "thing two";
+  const itemToNotDelete = "thing three";
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DialogUtComponent ]
-    })
-    .compileComponents();
+      declarations: [DialogUtComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +23,57 @@ describe('ui-noninteractive - DialogUtComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+
+  // we would have more tests here, focused on the data we get and display
+  // We're focused on the dialog here, so will not explore those tests
+
+  // I have a list of something, with a delete button next to each item.
+  // I click delete, and get a dialog "Are you sure you want to delete {{ item.name }}?"
+  // If I click delete, the item is removed from the list.
+  //
+  // I need an array of data to present, and a "deleted item" as well as "Not deleted item"
+  // to test the scenario where I delete the data, and do not delete the data
+  it("should launch a alert dialog with a click of the delete button for a list item", () => {
+    fixture.whenStable().then(() => {
+      // let's get our list item!
+      const ourDomListUnderTest = document.querySelector("mat-list");
+
+      console.log("ourDomLIstUnderTest ", ourDomListUnderTest);
+      // now let's get the inner text, and filter out on this list item
+      // then pull the delete button
+
+      const listItemToDelete = Array.from(
+        ourDomListUnderTest.getElementsByTagName("mat-list-item")
+      ).filter(
+        element =>
+          element.getElementsByTagName("h4")[0].innerText === itemToDelete
+      );
+
+      console.log(listItemToDelete);
+
+      // there should only be one of these, so go for zero
+      const deleteButton = listItemToDelete[0].getElementsByTagName(
+        "button"
+      )[0];
+
+      // and click the button
+      deleteButton.click();
+      fixture.detectChanges();
+
+      // and we should have a dialog
+      const dialogDiv = document.querySelector("mat-dialog-container");
+      expect(dialogDiv).toBeTruthy();
+    });
+  });
+
+  it("should delete a list item when the dialog is confirmed", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should not delete a list item when the dialog is rejected", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/ui-noninteractive-tests/dialog-ut/dialog-ut.component.ts
+++ src/app/ui-noninteractive-tests/dialog-ut/dialog-ut.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dialog-ut',
-  templateUrl: './dialog-ut.component.html',
-  styleUrls: ['./dialog-ut.component.css']
+  selector: "app-dialog-ut",
+  templateUrl: "./dialog-ut.component.html",
+  styleUrls: ["./dialog-ut.component.css"]
 })
 export class DialogUtComponent implements OnInit {
+  ourItems = ["thing one", "thing two", "thing three"];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
+  
+  getThings() {}
 }
