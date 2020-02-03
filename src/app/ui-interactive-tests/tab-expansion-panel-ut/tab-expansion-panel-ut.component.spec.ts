850982efad55eb3f02a38d104290dce7d9b2cada
Jason Woods <jason.woods@datatri.be>
2020-02-03 06:26:05 -0600

    getting chicken tab test set up

--- src/app/ui-interactive-tests/tab-expansion-panel-ut/tab-expansion-panel-ut.component.spec.ts
+++ src/app/ui-interactive-tests/tab-expansion-panel-ut/tab-expansion-panel-ut.component.spec.ts
@@ -12,18 +12,42 @@ describe('TabExpansionPanelUtComponent', () => {
   const expectedTabLabels = ['Beef', 'Chicken', 'Pork'];
 
   const beefMenu = [
-    { name: 'Filet', description: 'Yummy', img: '' },
-    { name: 'T-bone', description: 'Good', img: '' }
+    {
+      name: 'Filet',
+      description: 'Yummy',
+      img: ''
+    },
+    {
+      name: 'T-bone',
+      description: 'Good',
+      img: ''
+    }
   ];
 
   const chickenMenu = [
-    { name: 'Grilled breast', description: 'Yummy', img: '' },
-    { name: 'Fried drumsticks', description: 'Good', img: '' }
+    {
+      name: 'Grilleds breast',
+      description: 'Yummy',
+      img: ''
+    },
+    {
+      name: 'Fried drumsticks',
+      description: 'Good',
+      img: ''
+    }
   ];
 
   const porkMenu = [
-    { name: 'Tenderloin Medalliion', description: 'Yummy', img: '' },
-    { name: 'Steak', description: 'Good', img: '' }
+    {
+      name: 'Tenderloin Medalliion',
+      description: 'Yummy',
+      img: ''
+    },
+    {
+      name: 'Steak',
+      description: 'Good',
+      img: ''
+    }
   ];
 
   beforeEach(async(() => {
@@ -56,21 +80,77 @@ describe('TabExpansionPanelUtComponent', () => {
     });
   });
 
-  // Do we have the right data in the tabs?
+  // Do we have the right data in the beef tab?
   it('should have the correct data in each tab', () => {
-    fixture.whenRenderingDone().then(() => {
-      const tabLabels = document.querySelectorAll(
-        '#menu .mat-tab-label-content'
+    // mat-tab-header will have ng-reflect-selected-index
+    // using our id, we can dive right into the elements we want to see
+    const expansionPanels = document.querySelectorAll(
+      'mat-accordion#beef mat-expansion-panel'
+    );
+
+    Array.from(expansionPanels).forEach(element => {
+      const headerTitle = element.querySelector(
+        'mat-expansion-panel-header mat-panel-title'
+      );
+      const headerDescription = element.querySelector(
+        'mat-expansion-panel-header mat-panel-description'
       );
 
-      Array.from(tabLabels).forEach(element => {
-        expect(expectedTabLabels).toContain(element.textContent);
-      });
+      const content = element.querySelector('div .mat-expansion-panel-body');
+
+      expect(beefMenu).toContain(
+        jasmine.objectContaining({
+          // Trim this content! There might be whitespace in front or back of the content to make it look good
+          name: headerTitle.textContent.trim(),
+          description: headerDescription.textContent.trim(),
+          img: content.textContent.trim()
+        })
+      );
     });
   });
 
-  // Do we have the correct data in the expansion panel?
+  // Do we have the right data in the chicken tab?
   it('should have the correct data in each tab', () => {
-    expect(component).toBeTruthy();
+    // mat-tab-header will have ng-reflect-selected-index
+    // using our id, we can dive right into the elements we want to see
+    // const tabs = document.getElementsByClassName('mat-tab-label-content');
+
+    // chicken is at index 1, so 0-1
+    const labelContainer = document.getElementsByClassName('mat-tab-labels')[0];
+
+    labelContainer.childNodes.forEach(element => {
+      if (element.textContent === expectedTabLabels[1]) {
+        console.log('the element ', element);
+        element.dispatchEvent(new MouseEvent('mousedown'));
+        element.firstChild.dispatchEvent(new MouseEvent('click'));
+        console.log('the element after ', element);
+      }
+    });
+
+    const expansionPanels = document.querySelectorAll(
+      'mat-accordion#chicken mat-expansion-panel'
+    );
+
+    console.log('expansion panels ', expansionPanels);
+
+    Array.from(expansionPanels).forEach(element => {
+      const headerTitle = element.querySelector(
+        'mat-expansion-panel-header mat-panel-title'
+      );
+      const headerDescription = element.querySelector(
+        'mat-expansion-panel-header mat-panel-description'
+      );
+
+      const content = element.querySelector('div .mat-expansion-panel-body');
+
+      expect(chickenMenu).toContain(
+        jasmine.objectContaining({
+          // Trim this content! There might be whitespace in front or back of the content to make it look good
+          name: headerTitle.textContent.trim(),
+          description: headerDescription.textContent.trim(),
+          img: content.textContent.trim()
+        })
+      );
+    });
   });
 });
