c54ef366fdf55101c1e07649a3a193c26fe61580
Jason Woods <jason.woods@datatri.be>
2020-02-27 06:34:58 -0600

    Fiddling with dispatching very custom mouse events for the slider tests

--- src/app/ui-interactive-tests/slide-toggle-slider-ut/slide-toggle-slider-ut.component.spec.ts
+++ src/app/ui-interactive-tests/slide-toggle-slider-ut/slide-toggle-slider-ut.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SlideToggleSliderUtComponent } from './slide-toggle-slider-ut.component';
+import { DebugElement } from '@angular/core';
+import {
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatCardModule
+} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('SlideToggleSliderUtComponent', () => {
   let component: SlideToggleSliderUtComponent;
@@ -8,9 +15,15 @@ describe('SlideToggleSliderUtComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SlideToggleSliderUtComponent ]
-    })
-    .compileComponents();
+      declarations: [SlideToggleSliderUtComponent],
+      imports: [
+        MatSliderModule,
+        MatSlideToggleModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatCardModule
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -20,6 +33,48 @@ describe('SlideToggleSliderUtComponent', () => {
   });
 
   it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should change the color of the color selection when the slider is adjusted', () => {
+    // be sure the changeColorValue method is called when the slider is moved
+    const sliderDE: DebugElement = fixture.debugElement;
+    const sliderEL: HTMLElement = sliderDE.nativeElement;
+    const slider = sliderEL.querySelector('mat-slider#redSlider');
+
+    const spy = spyOn(component, 'changeColorValue');
+
+    const trackElement = slider.querySelector('.mat-slider-wrapper')!;
+    const dimensions = trackElement.getBoundingClientRect();
+    const x = dimensions.left + dimensions.width * 0.18;
+    const y = dimensions.top + dimensions.height * 0.18;
+
+    const event = document.createEvent('MouseEvent');
+    event.initMouseEvent('mousedown',
+      true, /* canBubble */
+      true, /* cancelable */
+      window, /* view */
+      0, /* detail */
+      x, /* screenX */
+      y, /* screenY */
+      x, /* clientX */
+      y, /* clientY */
+      false, /* ctrlKey */
+      false, /* altKey */
+      false, /* shiftKey */
+      false, /* metaKey */
+      0, /* button */
+      null /* relatedTarget */);
+    slider.dispatchEvent(event);
+    fixture.detectChanges();
+
+    console.log(slider.getAttribute('ng-reflect-model'));
+
+    // be sure the color of the mat-card is adjusted when above
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should lock the color selection when the slider is switched', () => {
     expect(component).toBeTruthy();
   });
 });
