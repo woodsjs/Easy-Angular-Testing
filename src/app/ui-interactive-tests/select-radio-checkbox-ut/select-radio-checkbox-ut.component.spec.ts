dd37fc8de617b3808dfcd9833ce9ef019e422427
Jason Woods <jason.woods@datatri.be>
2020-03-18 06:50:26 -0500

    Have our initial select test done. Working on meat/non-meat filter

--- src/app/ui-interactive-tests/select-radio-checkbox-ut/select-radio-checkbox-ut.component.spec.ts
+++ src/app/ui-interactive-tests/select-radio-checkbox-ut/select-radio-checkbox-ut.component.spec.ts
@@ -1,19 +1,45 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  fakeAsync
+} from '@angular/core/testing';
 
 import { SelectRadioCheckboxUtComponent } from './select-radio-checkbox-ut.component';
-import { MatRadioModule, MatCheckboxModule, MatSelectModule } from '@angular/material';
+import {
+  MatRadioModule,
+  MatCheckboxModule,
+  MatSelectModule
+} from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
 
 describe('SelectRadioCheckboxUtComponent', () => {
   let component: SelectRadioCheckboxUtComponent;
   let fixture: ComponentFixture<SelectRadioCheckboxUtComponent>;
 
+  const sandwichList = [
+    'veggie',
+    'beef',
+    'grilledChicken',
+    'grilledFish',
+    'chickenSalad',
+    'tunaSalad'
+  ];
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SelectRadioCheckboxUtComponent ],
-      imports: [MatRadioModule, MatCheckboxModule, MatSelectModule, FormsModule, ReactiveFormsModule]
-    })
-    .compileComponents();
+      declarations: [SelectRadioCheckboxUtComponent],
+      imports: [
+        MatRadioModule,
+        MatCheckboxModule,
+        MatSelectModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -25,4 +51,96 @@ describe('SelectRadioCheckboxUtComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // This is considered a listbox. Did you know?
+  // mat-select (id=mat-select-0) -> mat-select-trigger -> mat-select-trigger -> mat-select-value
+  // mat-select-value-text
+  // cdk-overlay-container -> div.cdk-overlay-0 ->
+  // div.mat-select-panel-wrap -> mat-select-panel -> mat-option(value has the text)
+  it('should populate our sandwich select from a list in code', async () => {
+    const trigger = fixture.debugElement.query(By.css('.mat-select-trigger'))
+      .nativeElement;
+
+    trigger.click();
+    fixture.detectChanges();
+
+    await fixture.whenStable().then(() => {
+      const inquiryOptions = fixture.debugElement.query(
+        By.css('.mat-select-panel')
+      ).nativeElement;
+
+      for (const option of inquiryOptions.children) {
+        // TODO: let's move our bigger list here, and use map reduce to get the data we need for this test
+        expect(sandwichList).toContain(option.getAttribute('ng-reflect-value'));
+      }
+    });
+  });
+
+  // mat-checkbox -> label -> div -> input
+  it('should display the appropriate extra options for sandwich type', () => {
+    // TODO: Move bigger sandwichList here and use map reduce to get the data we need (meat vs nonmeat and name)
+    // loop through the options, click on each, and verify that the correct options show up
+    expect(component).toBeTruthy();
+  });
+
+  // mat-radio-group -> mat-radio-button
+  it('should populate our sides from a list in code', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should submit the proper data when order is clicked', () => {
+    expect(component).toBeTruthy();
+  });
 });
+
+// dispatchKeyboardEvent(select, 'keydown', DOWN_ARROW);
+
+// expect(options[0].selected).toBe(true, 'Expected first option to be selected.');
+// expect(formControl.value).toBe(
+//   options[0].value,
+//   'Expected value from first option to have been set on the model.'
+// );
+
+// dispatchKeyboardEvent(select, 'keydown', DOWN_ARROW);
+// dispatchKeyboardEvent(select, 'keydown', DOWN_ARROW);
+
+// // Note that the third option is skipped, because it is disabled.
+// expect(options[3].selected).toBe(
+//   true,
+//   'Expected fourth option to be selected.'
+// );
+// expect(formControl.value).toBe(
+//   options[3].value,
+//   'Expected value from fourth option to have been set on the model.'
+// );
+
+// dispatchKeyboardEvent(select, 'keydown', UP_ARROW);
+
+// expect(options[1].selected).toBe(
+//   true,
+//   'Expected second option to be selected.'
+// );
+// expect(formControl.value).toBe(
+//   options[1].value,
+//   'Expected value from second option to have been set on the model.'
+// );
+
+// here's a click
+//  fixture.componentInstance.select.open();
+//  fixture.detectChanges();
+//  flush();
+
+//  (overlayContainerElement.querySelectorAll(
+//    'mat-option'
+//  )[3] as HTMLElement).click();
+//  fixture.detectChanges();
+//  flush();
+
+//  expect(formControl.value).toBe(options[3].value);
+
+//  dispatchKeyboardEvent(select, 'keydown', DOWN_ARROW);
+//  fixture.detectChanges();
+
+//  expect(formControl.value).toBe(options[4].value);
+
+// https://github.com/angular/components/blob/master/src/material/select/select.spec.ts
--- src/app/ui-interactive-tests/select-radio-checkbox-ut/select-radio-checkbox-ut.component.ts
+++ src/app/ui-interactive-tests/select-radio-checkbox-ut/select-radio-checkbox-ut.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { not } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-select-radio-checkbox-ut',
@@ -9,12 +10,26 @@ import { FormGroup, FormControl, FormArray } from '@angular/forms';
 export class SelectRadioCheckboxUtComponent implements OnInit {
   sides: string[] = ['Chips', 'Fruit', 'Salad', 'None'];
   extraList = [
-    { id: 1, text: 'Meat' },
+    { id: 1, text: 'Xtra Meat' },
     { id: 10, text: 'Avocado' },
     { id: 11, text: 'Cilantro' }
   ];
+  extrasAvailable = [];
 
+  // This would come from an API! We don't want to have to make a code change
+  // to add an item
+  sandwichList = [
+    { name: 'veggie', text: 'Veggie', type: 'nonMeat' },
+    { name: 'beef', text: 'Beef', type: 'meat' },
+    { name: 'grilledChicken', text: 'Grilled Chicken', type: 'meat' },
+    { name: 'grilledFish', text: 'Grilled Fish', type: 'meat' },
+    { name: 'chickenSalad', text: 'Chicken Salad', type: 'meat' },
+    { name: 'tunaSalad', text: 'Tuna Salad', type: 'meat' }
+  ];
+
+  nonMeat = ['veggie'];
   sandwichFormGroup;
+  sandwichOrder;
 
   constructor() {}
 
@@ -29,18 +44,45 @@ export class SelectRadioCheckboxUtComponent implements OnInit {
   }
 
   private addCheckboxes() {
+    // clear our extras before we reFill it
+    this.extrasAvailable.splice(0, this.extrasAvailable.length);
+
+    // let's add map-reduce to get our non-meat items to use here
     this.extraList.forEach((o, i) => {
-      const control = new FormControl(); // if first item set to true, else false
-      (this.sandwichFormGroup.controls.extras as FormArray).push(control);
+      const sandwichType = this.sandwichFormGroup.controls.sandwich.value;
+      // we need to trigger checkboxes when a sandwich is selected
+      // if it's a non-meat sammy, don't have the xtra meat option
+
+      if (this.nonMeat.includes(sandwichType) === false) {
+        const control = new FormControl();
+        (this.sandwichFormGroup.controls.extras as FormArray).push(control);
+        this.extrasAvailable.push({ id: o.id, text: o.text });
+      } else {
+        if ([1].indexOf(o.id) < 0) {
+          const control = new FormControl();
+          (this.sandwichFormGroup.controls.extras as FormArray).push(control);
+          this.extrasAvailable.push({ id: o.id, text: o.text });
+        }
+      }
     });
   }
 
-  onSubmit() {
-    console.log('the output ', this.sandwichFormGroup);
+  selectionChanged(selected) {
+    // we can use this on ng 8
+    // (this.sandwichFormGroup.controls.extras as FormArray).clear();
+    while (this.sandwichFormGroup.controls.extras.length > 0) {
+      this.sandwichFormGroup.controls.extras.removeAt(0);
+    }
 
+    this.addCheckboxes();
+  }
+
+  onSubmit() {
     const selectedOrderIds = this.sandwichFormGroup.value.extras
       .map((v, i) => (v ? this.extraList[i].id : null))
       .filter(v => v !== null);
-    console.log(selectedOrderIds);
+
+    this.sandwichOrder = this.sandwichFormGroup.value;
+    console.log('the sandwich order ', this.sandwichOrder);
   }
 }
