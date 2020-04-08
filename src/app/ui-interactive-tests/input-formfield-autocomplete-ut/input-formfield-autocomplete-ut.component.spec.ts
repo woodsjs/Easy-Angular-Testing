5e585b6f9c6e166a6a26e3bc23cc53f70a10980b
Jason Woods <jason.woods@datatri.be>
2020-04-08 07:15:56 -0500

    Test to check our autocomplete fills is done. Need to select an option and check results now

--- src/app/ui-interactive-tests/input-formfield-autocomplete-ut/input-formfield-autocomplete-ut.component.spec.ts
+++ src/app/ui-interactive-tests/input-formfield-autocomplete-ut/input-formfield-autocomplete-ut.component.spec.ts
@@ -1,14 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InputFormfieldAutocompleteUtComponent } from './input-formfield-autocomplete-ut.component';
-import { MatInputModule, MatFormFieldModule, MatAutocompleteModule } from '@angular/material';
+import {
+  MatInputModule,
+  MatFormFieldModule,
+  MatAutocompleteModule,
+} from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
+import { By } from '@angular/platform-browser';
+import { FixedSizeVirtualScrollStrategy } from '@angular/cdk/scrolling';
 
 describe('InputFormfieldAutocompleteUtComponent', () => {
   let component: InputFormfieldAutocompleteUtComponent;
   let fixture: ComponentFixture<InputFormfieldAutocompleteUtComponent>;
+
+  const testState = {
+    state: 'Illinois',
+    city: ['Chicago', 'Aurora', 'Naperville', 'Joliet'],
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,9 +30,9 @@ describe('InputFormfieldAutocompleteUtComponent', () => {
         MatFormFieldModule,
         BrowserAnimationsModule,
         MatAutocompleteModule,
-        CommonModule
+        CommonModule,
       ],
-      declarations: [InputFormfieldAutocompleteUtComponent]
+      declarations: [InputFormfieldAutocompleteUtComponent],
     }).compileComponents();
   }));
 
@@ -38,15 +49,51 @@ describe('InputFormfieldAutocompleteUtComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should activate submit button if required fields are filled', () => {
-    expect(component).toBeTruthy();
-  });
+  // cdk-overlay-container -> cdk-overlay-N -> div.mat-autocomplete-panel -> mat-option -> span.mat-option-text
+  // this is just like the mat-select
+  it('should poplate the state input with the proper autocomplete options', async () => {
+    const stateOptions = component.options.map((option) => option.state);
 
-  it('should poplate the state input with the proper autocomplete options', () => {
-    expect(component).toBeTruthy();
+    const stateClicker = fixture.debugElement.query(
+      By.css('mat-form-field#state input')
+    ).nativeElement;
+
+    stateClicker.click();
+    fixture.detectChanges();
+
+    await fixture.whenStable().then(() => {
+      const autocompleteOptions = fixture.debugElement.query(
+        By.css('.mat-autocomplete-panel')
+      ).nativeElement;
+
+      for (const option of autocompleteOptions.children) {
+        expect(stateOptions).toContain(
+          option.getAttribute('ng-reflect-value')
+        );
+      }
+    });
   });
 
   it('should properly populate the city list when our state is chosen', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should activate submit button if required fields are filled', () => {
+    const nElement = fixture.debugElement.nativeElement;
+
+    // let's get our input with the autocomplete
+
+    // and click in it
+
+    // and get our cdk container for the mat-autocomplete
+
+    // and type the state name
+
+    // now get the cities related to that state
+
+    // and enter one in the City box
+
+    // now check that our submit button is good
     expect(component).toBeTruthy();
   });
 
