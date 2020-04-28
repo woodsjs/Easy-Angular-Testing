f655dadffa645edf5f147a56364cc7c0f2453716
Jason Woods <jason.woods@datatri.be>
2020-04-28 05:56:11 -0500

    Finished tests. No optimization done.

--- src/app/ui-interactive-tests/datepicker-navcontainers-ut/datepicker-navcontainers-ut.component.spec.ts
+++ src/app/ui-interactive-tests/datepicker-navcontainers-ut/datepicker-navcontainers-ut.component.spec.ts
@@ -86,17 +86,52 @@ describe('DatepickerNavcontainersUtComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  // if you don't want to do all this, you can probably follow the model from
+  // the material tests and do
+  // testComponent.selected = new Date(2017, JAN, 1);
+  // fixture.detectChanges();
+  // This is much more efficient, but not as fun to trace through :-)
   it('should change the text when a date is chosen from the datepicker', () => {
-    // get the datepicker
+    // get the datepicker toggle, which contains a button, then get the button
+    const mainDatePicker = fixture.debugElement.query(
+      By.css('mat-datepicker-toggle#mainpicker button')
+    ).nativeElement;
 
-    // get the cdk options
+    mainDatePicker.dispatchEvent(new MouseEvent('click'));
+    fixture.detectChanges();
 
-    // select the options
+    // our body cells - we're in a year view first so we get the years
+    const yearCells = fixture.debugElement.queryAll(
+      By.css('.mat-calendar-body-cell')
+    );
 
-    // get the text from the mat-sidenav-content
+    // and we're fine with clicking any year
+    yearCells[0].nativeElement.click();
+    fixture.detectChanges();
+
+    // now our table changed to month view, so get the first month
+    const monthCells = fixture.debugElement.queryAll(
+      By.css('.mat-calendar-body-cell')
+    );
+    // again clicking on any cell In this case, select the second. We want february since jan is the default
+    monthCells[1].nativeElement.click();
+    fixture.detectChanges();
+
+    // now we would have our day view, so get the first day
+    const dayCells = fixture.debugElement.queryAll(
+      By.css('.mat-calendar-body-cell')
+    );
+
+    // again clicking on any cell
+    dayCells[0].nativeElement.click();
+    fixture.detectChanges();
+
+    const mainContentText = fixture.debugElement.query(
+      By.css('mat-sidenav-content span')
+    ).nativeElement;
 
     // check that the text matches teh expected
-    expect(component).toBeTruthy();
+    expect(mainContentText.innerText).toEqual(monthValues[1].cheese);
   });
 
   // mat-sidenav hierarchy is mat-sidenav -> div.mat-drawer-inner-container -> elements (in our case buttons)
@@ -111,14 +146,45 @@ describe('DatepickerNavcontainersUtComponent', () => {
       element.click();
       fixture.detectChanges();
 
-      const mainContentText = fixture.debugElement.query(By.css('mat-sidenav-content span')).nativeElement;
-      const expectedCheese = monthValues.filter((value) => value.name === element.getAttribute('id'));
+      const mainContentText = fixture.debugElement.query(
+        By.css('mat-sidenav-content span')
+      ).nativeElement;
+      const expectedCheese = monthValues.filter(
+        (value) => value.name === element.getAttribute('id')
+      );
 
       expect(mainContentText.innerText).toEqual(expectedCheese[0].cheese);
     }
   });
 
+  // we can check things a few ways.
+  // I would typically put each of the expect statements into their own tests
+  // but leave them all together here for brevity
   it('should add the buttons we expect to the mat-sidenav container', () => {
-    expect(component).toBeTruthy();
+    const sidenavContainer = fixture.debugElement.query(
+      By.css('mat-sidenav div.mat-drawer-inner-container')
+    ).nativeElement;
+
+    const arrayOfButtons = [];
+
+    // grab all our buttons and save them in a list for a later test
+    // and while we're on it check that this rendered button is in the list of expected items
+    for (const element of sidenavContainer.children) {
+      const expectedButtons = monthValues.filter(
+        (value) => value.name === element.getAttribute('id')
+      );
+
+      arrayOfButtons.push(element.getAttribute('id'));
+      expect(element.getAttribute('id')).toEqual(expectedButtons[0].name);
+    }
+
+    // we can check the lengths of our arrays too!
+    expect(arrayOfButtons.length).toEqual(monthValues.length);
+
+    // check our expected list against the button list
+    // so we're checking in the opposite direction than the above test
+    for (const expectedMonth of monthValues) {
+      expect(arrayOfButtons).toContain(expectedMonth.name);
+    }
   });
 });
